f09e25e5cb8d25228e1ffe54a333fc74b6964f98
herb110 <1847003418@qq.com>
2017-10-05 21:19:44 +0800

    修改views.py,settings.py,urls.py

--- mainsite/views.py
+++ mainsite/views.py
@@ -2,19 +2,32 @@
 from __future__ import unicode_literals
 
 import sys
+
+from django.template.loader import get_template
 from django.shortcuts import render
 from django.http import HttpResponse
+from datetime import datetime
 from .models import Post
+from django.shortcuts import redirect
 
 reload(sys)
 sys.setdefaultencoding('utf-8')
 
 # Create your views here.
 def homepage(request):
+	template = get_template('index.html')
 	posts = Post.objects.all()
-	post_lists = list()
-	for count,post in enumerate(posts):
-		post_lists.append("No.{}:".format(str(count)) + str(post) +"<hr>")
-		post_lists.append("<small>" + str(post.body.encode('utf-8')) +  "</small><br><br>")
+	now = datetime.now()
+	html = template.render(locals())
+	return HttpResponse(html)
 
-	return HttpResponse(post_lists)
+def showpost(request, slug):
+    template = get_template('post.html')
+    try:
+        post = Post.objects.get(slug=slug)
+        if post != None:
+            html = template.render(locals())
+            return HttpResponse(html)
+    except :
+        return redirect('/')
+        
