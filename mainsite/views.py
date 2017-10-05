0b81e5180896aa10fc73c04dc778493dc4ea9cb8
herb110 <1847003418@qq.com>
2017-10-05 02:14:28 -0700

    commit

--- mainsite/views.py
+++ mainsite/views.py
@@ -1,6 +1,20 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
+import sys
 from django.shortcuts import render
+from django.http import HttpResponse
+from .models import Post
+
+reload(sys)
+sys.setdefaultencoding('utf-8')
 
 # Create your views here.
+def homepage(request):
+	posts = Post.objects.all()
+	post_lists = list()
+	for count,post in enumerate(posts):
+		post_lists.append("No.{}:".format(str(count)) + str(post) +"<hr>")
+		post_lists.append("<small>" + str(post.body.encode('utf-8')) +  "</small><br><br>")
+
+	return HttpResponse(post_lists)
--- mblog/settings.py
+++ mblog/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '4_z*t%3b)jukb%75$lp%4ff&tw88w8n0#-g83qesfg#id$*pbd'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'mainsite',
 ]
 
 MIDDLEWARE = [
@@ -103,9 +104,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/1.11/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'zh-Hans'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Shanghai'
 
 USE_I18N = True
 
